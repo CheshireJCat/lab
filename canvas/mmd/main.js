f57eb9b1844e28829d03c47a783bfb25ecc00c53
cheshireCat <lmaomaoz@gmail.com>
2016-07-26 16:57:11 +0800

    add mmd

---
+++ canvas/mmd/main.js
@@ -0,0 +1,103 @@
+tm.main(function() {
+    var app = tm.hybrid.Application("#canvas2d", "#canvas3d");
+    app.fps = 60;
+    app.resize(640, 960).fitWindow().run();
+    app.enableStats();
+    
+    app.replaceScene(tm.game.LoadingScene({
+        width: 640, height: 960,
+        assets: {
+            hiyoko: "assets/hiyoco_nomal_full.png",
+            bgm: "assets/jljt.mp3",
+
+            //urlをモデルデータ、モーションデータの順に指定
+            //There is a need to specify the PMD and VMD together to play the MMD
+            miku: {
+                type: "mmd",
+                url: ["assets/pmd/miku_v2.pmd", "assets/pmd/jl2.vmd"],
+            },
+            //分けて読み込みも可能
+            neru:   "assets/pmd/miku.pmd",
+            wave:   "assets/pmd/jl1.vmd",
+        },
+        nextScene: MikuOnStage,
+    }));
+});
+
+tm.define("MikuOnStage", {
+    superClass: "tm.hybrid.Scene", // tm.app.Sceneの3D向け拡張
+    init: function() {
+        this.superInit();
+
+        // カメラ調整
+        this.camera.setPosition(0, 20, 30);
+        this.camera.lookAt(new THREE.Vector3(0, 10, 0));
+        
+        // ライトを動かす
+        this.directionalLight.setPosition(0, 100, -80);
+
+        // メッシュを表示する
+        var miku = tm.hybrid.MMDMesh("miku")
+            .addChildTo(this)
+            .setPosition(0, 0, 0)
+            .on("enterframe", function() {
+                if (this.rolling) this.rotationY += 10; // Y軸回転
+            });
+
+        //分割読み込みからメッシュを生成
+        var neru = tm.hybrid.createMeshFromMMD("neru", "wave")
+            .addChildTo(this)
+            .setPosition(0, 0, -30)
+            .setRotation(0, 180, 0)
+            .on("enterframe", function() {
+                if (this.rolling) this.rotationY += 10; // Y軸回転
+            });
+
+        // 2Dスプライトとの併用も可能
+        var hiyoko = tm.display.Sprite("hiyoko", 32, 32)
+            .setScale(4)
+            .setFrameIndex(0)
+//            .addChildTo(this)
+            .on("enterframe", function() {
+                this.x += this.vx * 10;
+                this.y += this.vy * 10;
+                if (this.x < 0 || 640 < this.x) this.vx *= -1;
+                if (this.y < 0 || 960 < this.y) this.vy *= -1;
+                
+                this.frameIndex = (this.frameIndex + 1) % 4;
+                this.rotation += 2;
+            });
+        hiyoko.vx = 1;
+        hiyoko.vy = 1;
+
+        var btn = tm.ui.FlatButton({ text: "スタート" })
+            .setPosition(320, 960*0.5)
+            .addChildTo(this)
+            .on("push", function() {
+                btn.visible = false;
+            }.bind(this));
+
+        this.bgm = tm.asset.AssetManager.get("bgm");
+        THREE.AnimationHandler.update(0);
+
+        this.start = false;
+
+        this.time = 0;
+    },
+    update: function(app) {
+        if (!this.start) return;
+        var t = this.bgm.context.currentTime;
+        var delta = t - this.beforeTime;
+        THREE.AnimationHandler.update(app.deltaTime/1000);
+        this.beforeTime = t;
+    },
+    ontouchstart: function(e) {
+        if (!this.start) {
+            tm.sound.WebAudio.unlock();
+            this.bgm.context.currentTime = 0;
+            this.bgm.play();
+            this.start = true;
+            this.beforeTime = 0;
+        }
+    },
+});
---
+++ canvas/mmd/mmdmesh.js
@@ -0,0 +1,82 @@
+/*
+ * mmdmesh.js
+ */
+
+(function() {
+    // var tm = require("../../../libs/tmlib");
+    // var THREE = require("../../../libs/three");
+    // require("./delegateutil");
+    // require("./threeelement");
+
+    tm.define("tm.hybrid.MMDMesh", {
+        superClass: "tm.hybrid.ThreeElement",
+
+        init: function(mesh) {
+            if (typeof(mesh) === "string") {
+                var asset = tm.asset.Manager.get(mesh);
+                console.log(tm.asset)
+                if (asset) {
+                    if (asset instanceof tm.asset.ThreeJSON) {
+                        this.superInit(asset.mesh.clone());
+                    } else if (asset instanceof tm.asset.MQO) {
+                        this.superInit(asset.model.meshes[0]);
+                        for (var i = 1; i < asset.model.meshes.length; i++) {
+                            tm.hybrid.Mesh(asset.model.meshes[i]).addChildTo(this);
+                        }
+                    } else if (asset instanceof tm.asset.PMD) {
+                        var loader = new THREE.MMDLoader();
+                        var mesh = loader.createMesh(asset.data, undefined, asset.pmd.texturePath, function(){}, function(){});
+                        this.superInit(mesh);
+                    } else if (asset instanceof tm.asset.MMD) {
+                        var mesh = asset.mesh.clone();
+                        this.superInit(mesh);
+                        this._animation = new THREE.Animation(mesh, mesh.geometry.animation);
+                        this._animation.play();
+                        this._morphAnimation = new THREE.MorphAnimation2(mesh, mesh.geometry.morphAnimation);
+                        this._morphAnimation.play();
+                        this._ikSolver = new tm.hybrid.mmd.CCDIKSolver(mesh);
+                        var that = this;
+                        this.on('enterframe', function(e) {
+                            that._ikSolver.update();
+                        });
+                    }
+                } else {
+                    console.error("アセット'{0}'がないよ".format(mesh));
+                }
+            } else if (mesh instanceof THREE.Mesh) {
+                this.superInit(mesh);
+            } else if (mesh instanceof THREE.Geometry) {
+                if (arguments.length >= 2) {
+                    this.superInit(new THREE.Mesh(mesh, arguments[1]));
+                } else {
+                    this.superInit(new THREE.Mesh(mesh));
+                }
+            } else {
+                this.superInit(new THREE.Mesh());
+            }
+        },
+
+        playAnimation: function(startTime, weight) {
+            if (this._animation && this._morphAnimation) {
+                this._animation.play(startTime, weight);
+                this._morphAnimation.play(startTime);
+            }
+        },
+
+        stopAnimation: function() {
+            if (this._animation && this._morphAnimation) {
+                this._animation.stop();
+                this._morphAnimation.stop();
+            }
+        },
+    });
+
+    var delegater = tm.hybrid.DelegateUtil(tm.hybrid.Mesh);
+
+    delegater.property("geometry");
+    delegater.property("material");
+
+    delegater.method("getMorphTargetIndexByName", true);
+    delegater.method("updateMorphTargets", true);
+
+})();
